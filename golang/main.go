0891c14669fb1843bfe2b75994903585c02c49ee
moralss <jeramoral@gmail.com>
2019-01-02 11:20:54 +0200

    created basic apis with golang

--- golang/main.go
+++ golang/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+"encoding/json"
+"log"
+"net/http"
+"github.com/gorilla/mux"
+
+)
 
 func main() {
-	age := 25;
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler).Methods("GET");
 
-	fmt.Println(age2);
+	r.HandleFunc("/api/{id:[0-9]+}", ReturnParams).Methods("GET");
+	log.Fatal(http.ListenAndServe(":3000" , r));
 }
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type" , "application/json")
+	json.NewEncoder(w).Encode("hello world");	
+}
+
+
+func ReturnParams(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type" ,"application/json")
+	params := mux.Vars(r)
+	json.NewEncoder(w).Encode(params);	
+}
+
 
 
